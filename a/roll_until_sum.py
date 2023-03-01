998069ddda995bf75ef7cdf96d9163fc2af34037
bwilkins96 <ben.jr.speed@gmail.com>
2023-03-01 11:22:54 -0500

    added comments for assignments

--- a/roll_until_sum.py
+++ a/roll_until_sum.py
@@ -3,24 +3,34 @@
 from random import randrange
 
 class Dice:
+    'A dice class designed for Maryville University'
+
     def __init__(self, num_dice):
+        """
+        Initializes list with n number of die values
+        e.g. Dice(2) produces an instance with 2 die values
+        """
         self.dice = [1] * num_dice
         self.roll()
 
     def get_dice(self):
+        'Returns a copy of the dice list'
         return self.dice[:]
 
     def roll(self):
+        'Changes the values in the dice list to random values between 1 and 6'
         for i in range(len(self.dice)):
             self.dice[i] = randrange(1, 7)
 
     def sum_dice(self):
+        'Returns the sum of all values in the dice list'
         sum = 0
         for die in self.get_dice():
             sum += die
         
         return sum
 
+# Gets and validates target sum input
 def get_target_sum():
     while True:
         try:
@@ -33,6 +43,7 @@ def get_target_sum():
 
     return target_sum
 
+# Prints result of rolling 2 die
 def print_results(dice):
     die_1, die_2 = dice.get_dice()
     print(f'Roll: {die_1} and {die_2}, sum is {dice.sum_dice()}')
@@ -41,11 +52,13 @@ def main():
     print('This program rolls two 6-sided dice until their sum is a given target value.')
     target_sum = get_target_sum()
 
+    # Initializes to 2 die and primes for while loop
     dice = Dice(2)
     rolls = 1
     print()
     print_results(dice)
 
+    # Rolls dice and prints results until target sum is reached
     while dice.sum_dice() != target_sum:
         dice.roll()
         rolls += 1
--- shape_painter/random_shapes.py
+++ shape_painter/random_shapes.py
@@ -2,6 +2,7 @@
 
 from random import random, randrange
 
+# gets and validates user input for file and number of shapes to make
 def get_input():
     file_name = input('Enter the drawing file name to create: ')
 
@@ -17,10 +18,12 @@ def get_input():
 
     return file_name, num_shapes
 
+# Returns a coordinates string using random coordinates 
 def get_random_points(name):
     max_height = 500
     max_width = 500
 
+    # Gets different coordinates based on shape name
     if name == 'Rectangle':
         coords = (randrange(1, max_height-1), randrange(1, max_width-1))
     else:
@@ -29,6 +32,7 @@ def get_random_points(name):
     
     return f'{coords[0]}, {coords[1]}'
 
+# Returns an rgb string with values between 192 and 255
 def get_random_color():
     r = randrange(192, 255) 
     g = randrange(192, 255)
@@ -36,6 +40,7 @@ def get_random_color():
 
     return f'{r}, {g}, {b}'
 
+# Returns a string specifying a rectangle
 def get_random_rect():
     name = 'Rectangle'
     ul_point = get_random_points(name)
@@ -44,6 +49,7 @@ def get_random_rect():
 
     return f'{name}; {ul_point}; {lr_point}; {color}'
 
+# Returns a string specifying a circle
 def get_random_circle():
     name = 'Circle'
     center_point = get_random_points(name)
@@ -52,20 +58,25 @@ def get_random_circle():
 
     return f'{name}; {center_point}; {radius}; {color}'
 
+# Main function that starts execution of program
 def main():
     file_name, num_shapes = get_input()
     shapes_file = open(file_name, 'w')
 
     for i in range(num_shapes):
+        # Randomly produces either a rectangle or circle string
         if random() < 0.5:
             shape_str = get_random_rect()
         else:
             shape_str = get_random_circle()
         
+        # Writes shape string to file
         print(shape_str, file=shapes_file)
 
     shapes_file.close()
     print(f'\nFinsihed writing to {file_name}')
+
+    # Returns file name for use in interface with 'shape_painter.py'
     return file_name
 
 if __name__ == '__main__': main()
